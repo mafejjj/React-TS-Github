163f5bf486fc88e92ebcd64635d9b353eb06a25b
Maria Fernanda de Jesus <mariafernandadejesus28@gmail.com>
2023-07-28 15:40:45 -0300

    projeto finalizado

--- github_finder/src/main.tsx
+++ github_finder/src/main.tsx
@@ -10,13 +10,20 @@ import Home from "./routes/Home";
 
 const router = createBrowserRouter ([
   {
-    
-  }
-])
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path:"/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
--- github_finder/src/routes/Home.tsx
+++ github_finder/src/routes/Home.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { UserProps } from "../components/types/user";
+
+import Search from "../components/Search";
+import User from "../components/User";
+import Error from "../components/Error";
+import {useState} from 'react';
 
 const Home = () => {
-    return <div>Home</div>;
+    const [user, setUser] = useState<UserProps | null>(null);
+    const [error, setError] = useState (false);
+
+    const loadUser = async(userName: string) => {
+        setError(false);
+        setUser(null);
+
+        const res = await fetch(`https://api.github.com/users/${userName}`)
+
+        const data = await res.json();
+
+        if(res.status === 404){
+            setError(true);
+            return;
+        }
+
+        const {avatar_url, login, location, followers, following} = data;
+
+        const userData: UserProps = {
+            avatar_url,
+            login,
+            location,
+            followers,
+            following,
+
+        };
+
+        setUser(userData);
+    };
+
+    return <div>
+        <Search loadUser={loadUser}/>
+        {user && <User {...user} />}
+        {error && <Error />}
+    </div>;
 };
 
 export default Home;
